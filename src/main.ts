96d59ce377cbe400a661c77ade77cd0ee5249072
Ivan <istepanchikov@scid.ru>
2025-12-18 13:12:43 +0300

    init

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+import "./assets/main.scss";
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import naive from 'naive-ui'
 import App from './App.vue'
 import router from './router'
 
@@ -8,5 +10,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(naive)
 app.mount('#app')
